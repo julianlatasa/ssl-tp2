cb50eda9ad0f595dcd4c2072387ec0f13e853b1c
Damián Decurgez <damian.decurgez@despegar.com>
2017-10-07 15:19:54 -0300

    Partial fixes...

--- tp3/main.c
+++ tp3/main.c
@@ -9,3 +9,47 @@
                Muscatiello Juan (156-098/0)
  ============================================================================
  */
+
+#include <stdio.h>
+#include <strings.h>
+#include "scanner.h"
+#include "tokens.h"
+
+char *token_names[] = {
+ 	"Programa",
+ 	"Variables",
+ 	"Código",
+ 	"Fin",
+ 	"Definir",
+ 	"Leer",
+ 	"Escribir",
+ 	"Identificador",
+ 	"Constante",
+ 	"Asignación",
+ 	"\'(\'", // Paréntesis izquierdo
+ 	"\')\'", // Paréntesis derecho
+ 	"\';\'", // Punto y coma
+ 	"\',\'", // Coma
+ 	"Comentario",
+ 	"\'+\'", // Más
+ 	"\'-\'", // Menos
+ 	"\'*\'", // Por
+ 	"\'/\'", // Dividir
+ 	"Fin de Archivo"
+};
+
+int main() {
+	enum token t;
+	while ((t = yylex()) != FDT){
+    switch(t){
+      case IDENT:
+      case CONST:
+        printf("Token: %s\t\tLexema: %s\n", token_names[t], yytext);
+        break;
+      default:
+        printf("Token: %s\n", token_names[t]);
+        break;
+    }
+	}
+	return 0;
+}
