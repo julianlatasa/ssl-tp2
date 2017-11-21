750e1dd0c1994237bf53a3731b386539220c3ca6
julianlatasa <julianlatasa@gmail.com>
2017-11-21 07:32:16 -0300

    Mejora en el uso de diccionario, se arreglaron cosas en rutina add_dict

--- tp5/semantic.c
+++ tp5/semantic.c
@@ -11,7 +11,7 @@ char * do_operation(char * operation, char * param1, char * param2){
   //printf("MULT %s, %s\n", $$, $3); $$ = $1;}
  char * name = "Temp#1"; // TODO: Concatenar Temp# con el número.
  printf("%s: %s %s \n", operation, param1, param2);
- add_dict(name);
+// add_dict(name);
 
  return name;
 }
--- tp5/symbol.c
+++ tp5/symbol.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "symbol.h"
+#include "parser.h"
 
 char *(dict[100]);
 static int pos = 0;
@@ -17,10 +18,17 @@ int add_dict(const char *nombre) {
 		dict[pos] = malloc(strlen(nombre)+1);
 		strcpy(dict[pos],nombre);
 		pos++;
-		printf("Guardado %s en la posición %d. \n", nombre, pos);
+		printf("Declare %s, Integer\n", nombre);
 		return 1;
 	}
-	printf("ERROR: %s ya existe en el diccionario. \n", nombre); //TODO: Usar yyerror.
+//	char *errormsg =  malloc(strlen(msg) + strlen(yytext) + 3);
+//	sprintf(errormsg, "%s: %s", msg, yytext);
+	// TODO: Se podria hacer una funcion, o ver si hay alguna, que le pasamos un mensaje y una variable y devuelve un string
+	// y se utilizaria aca y en el scanner.l
+	char *err_msg = malloc(2000);
+	sprintf(err_msg, "Error semantico: identificador %s ya declarado", nombre);
+	yyerror(err_msg);
+	free(err_msg);
 	return 0;
 }
 
